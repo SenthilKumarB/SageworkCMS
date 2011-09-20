3444fb350653457cb7c9d1b82b06fc299c18630f
Senthil Kuma Bhaskaran <senthil@sagework.com>
2011-09-20 20:04:57 +0530

    Minor CSS Fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110920104000) do
+ActiveRecord::Schema.define(:version => 20110920093618) do
 
   create_table "articles", :force => true do |t|
     t.integer  "user_id"
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(:version => 20110920104000) do
     t.time     "comment_time"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "title"
   end
 
   create_table "pages", :force => true do |t|
