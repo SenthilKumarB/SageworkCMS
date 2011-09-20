8bac151e3249506be91d4dd7769520b363e0c5b0
Senthil Kuma Bhaskaran <senthil@sagework.com>
2011-09-20 22:11:17 +0530

    Contact Form in Sagework CMS

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -6,12 +6,42 @@ class CategoryController < ApplicationController
   end
 
   def edit
+    @category = Category.find(params[:id])
+    @page_title = "Edit Category"
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(params[:category])
+      flash[:success] = "Successfully updated"
+      redirect_to :back
+    else
+      flash[:error] = "Failed in updating"
+      render :edit 
+    end
+
   end
 
   def show
   end
 
   def new
+    @page_title = "New Category"
+  end
+
+  def save
+    @category = Category.new(params[:category])
+    if @category.save
+      flash[:success] = "Successfully Saved"
+      redirect_to :back
+    else
+      flash[:error] = "Failed in Saving"
+      render :new
+    end
+  end
+
+  def list
+    @categories = Category.all
   end
 
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,4 +3,12 @@ class StaticController < ApplicationController
   	@page_title = "Site Map"
   end
 
+  def contact
+  	@page_title = "Contact"
+  end
+
+  def contact_email
+  	render :text => "#TODO Contact EMAIL"
+  end
+
 end
