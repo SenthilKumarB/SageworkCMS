fd0c2a9425a05a31d0829ccb095d220b011e118a
Senthil Kuma Bhaskaran <senthil@sagework.com>
2011-09-26 11:41:34 +0530

    css fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 
-  before_filter :require_user
+  # before_filter :require_user
 
   def index
   	@article = Article.find_all_by_show_home_page("1").last
